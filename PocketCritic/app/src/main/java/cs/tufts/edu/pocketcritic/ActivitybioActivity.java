96de5a5a491b8d4e48e93dcbf0abe34ce638be40
JoshuaW1990 <Jun.Wang@tufts.edu>
2016-09-28 16:43:25 -0400

    add function
    1. Add a new view named artist bio
    2. Add the interaction function of the buttons

--- PocketCritic/app/src/main/java/cs/tufts/edu/pocketcritic/ActivitybioActivity.java
+++ PocketCritic/app/src/main/java/cs/tufts/edu/pocketcritic/ActivitybioActivity.java
@@ -7,22 +7,20 @@ import android.content.Intent;
 import android.widget.Button;
 import android.app.Activity;
 
-
-
-public class MainActivity extends AppCompatActivity {
+public class ActivitybioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final Button button = (Button) findViewById(R.id.button1);
+        setContentView(R.layout.activitybio);
+
+        final Button button = (Button) findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
-                Intent launchactivity= new Intent(MainActivity.this,ActivitybioActivity.class);
+                Intent launchactivity= new Intent(ActivitybioActivity.this,MainActivity.class);
                 startActivity(launchactivity);
             }
         });
     }
-
 }
