f47dffb53d7073711e5af6610d20ff147e7e9f22
JoshuaW1990 <Jun.Wang@tufts.edu>
2016-10-11 10:41:43 -0400

    add a new login view for firebase

--- PocketCritic/app/src/main/java/cs/tufts/edu/pocketcritic/MainActivity.java
+++ PocketCritic/app/src/main/java/cs/tufts/edu/pocketcritic/MainActivity.java
@@ -15,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = (Button) findViewById(R.id.button5);
+        final Button button = (Button) findViewById(R.id.loginpage);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
-                Intent launchactivity= new Intent(MainActivity.this,ArtisitsActivity.class);
+                Intent launchactivity= new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(launchactivity);
             }
         });
