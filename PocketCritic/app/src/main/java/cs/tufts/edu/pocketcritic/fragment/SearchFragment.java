036cf184ffc8e728bc4b2fb303f624f4b3f98b7b
JoshuaW1990 <Jun.Wang@tufts.edu>
2016-11-03 18:24:47 -0400

    use retrofit to access data from spotify successfully

--- PocketCritic/app/src/main/java/cs/tufts/edu/pocketcritic/fragment/SearchFragment.java
+++ PocketCritic/app/src/main/java/cs/tufts/edu/pocketcritic/fragment/SearchFragment.java
@@ -11,6 +11,20 @@ import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+import cs.tufts.edu.pocketcritic.support.SpotifyInterface;
+import cs.tufts.edu.pocketcritic.support.SpotifyApi;
+import cs.tufts.edu.pocketcritic.model.Artist;
+
+import java.util.List;
 
 /**
  * Created by junwang on 11/3/16.
@@ -21,6 +35,9 @@ public class SearchFragment extends Fragment
 
     private EditText searchInfo;
     private Button searchButton;
+
+    private SpotifyApi spotifyApi;
+    private SpotifyInterface spotifyInterface;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,6 +52,9 @@ public class SearchFragment extends Fragment
         searchButton = (Button) view.findViewById(R.id.search_searchButton);
         searchButton.setOnClickListener(this);
 
+        spotifyApi = SpotifyApi.getApi();
+        spotifyInterface = spotifyApi.getService();
+
         return view;
 
     }
@@ -42,10 +62,83 @@ public class SearchFragment extends Fragment
     public void doSearch() {
         System.out.println("do search here by spotify");
         System.out.println(searchInfo.getText().toString());
+
+        query();
+
+
+    }
+
+    private String generateQueryString(String str) {
+        return str.replaceAll("\\s+","+");
+    }
+
+    private void queryByRxJava() {
+        String searchText = generateQueryString(searchInfo.getText().toString());
+        if (searchText.isEmpty()) {
+            Toast.makeText(getActivity(), "Please input artist name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        spotifyInterface.getSpotifyResult(searchText, "artist")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Artist>() {
+                    @Override
+                    public void onCompleted() {
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+
+                    @Override
+                    public void onNext(Artist result) {
+                        if (result != null) {
+                            List< Artist.ArtistsBean.ItemsBean > items = result.getArtists().getItems();
+                            for (Artist.ArtistsBean.ItemsBean item: items) {
+                                List<Artist.ArtistsBean.ItemsBean.ImagesBean> imageURLs = item.getImages();
+                                for(Artist.ArtistsBean.ItemsBean.ImagesBean image: imageURLs) {
+                                    String imageURL = image.getUrl();
+                                    System.out.println(imageURL);
+                                }
+                            }
+                        }
+                    }
+                });
     }
 
 
+    private void query() {
+        String searchText = generateQueryString(searchInfo.getText().toString());
+        System.out.println(searchText);
+        if (searchText.isEmpty()) {
+            Toast.makeText(getActivity(), "Please input artist name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Call<Artist> call = spotifyInterface.getResult(searchText, "artist");
+        call.enqueue(new Callback<Artist>() {
+            @Override
+            public void onResponse(Call<Artist> call, Response<Artist> response) {
+                if (response.isSuccessful()) {
+                    Artist result = response.body();
+                    if (result != null) {
+                        List< Artist.ArtistsBean.ItemsBean > items = result.getArtists().getItems();
+                        for (Artist.ArtistsBean.ItemsBean item: items) {
+                            List<Artist.ArtistsBean.ItemsBean.ImagesBean> imageURLs = item.getImages();
+                            for(Artist.ArtistsBean.ItemsBean.ImagesBean image: imageURLs) {
+                                String imageURL = image.getUrl();
+                                System.out.println(imageURL);
+                            }
+                        }
+                    }
+                }
+            }
 
+            @Override
+            public void onFailure(Call<Artist> call, Throwable t) {
+
+            }
+        });
+    }
 
 
     private void onSearchSuccess() {
